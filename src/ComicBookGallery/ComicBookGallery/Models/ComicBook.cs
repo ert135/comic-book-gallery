5570d3f64f2d49cc6934c79365b9eb98ec4a1036
Robert Smith <ert135@gmail.com>
2016-09-03 14:19:09 +0100

    added comic book model

---
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery.Models
+{
+    class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public string IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+    
+        public Artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
